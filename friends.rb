4dece874cc495cb03dd4e52664029bec8a58b8a5
Isaac Thill <ithill22@turing.edu>
2023-01-04 16:46:14 -0700

    Add places

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Bob", "Jim", "John", "Tom", "Bill"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places_to_visit = ["Rome", "Tokyo", "Berlin", "Paris", "Taipei"]
+places_to_visit.push("Mexico City")
+p places_to_visit
